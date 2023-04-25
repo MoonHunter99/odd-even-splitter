beefad4f752d25aa62f7adfcb3c957872329fb6a
Neil Daniel Vergara <vergaraneildaniel@gmail.com>
2023-04-25 15:44:48 +0800

    added decorations and time delay

--- odd_even_splitter.py
+++ odd_even_splitter.py
@@ -1,6 +1,10 @@
 #open the needed files
 with open("numbers.txt" , "r") as number_file, open("even.txt" , "a") as even_file, open("odd.txt" , "a") as odd_file:
     #for loops to read the lines
+    print("\033[1;34;40m ~" *134)
+    print("\033[1;34;40m Hello, this program will now check the numbers in the numbers.txt file")
+    import time
+    time.sleep(5)
     for line in number_file:
         #convert the lines to integeers
         number = int(line)
@@ -16,3 +20,5 @@ with open("numbers.txt" , "r") as number_file, open("even.txt" , "a") as even_fi
             odd = str(number)
             #write to even file
             odd_file.write(odd + "\n")
+    print("\033[1;34;40m Hello this program is now completed pls check the even.txt and odd.txt now")
+    print("\033[1;34;40m ~" *134)
