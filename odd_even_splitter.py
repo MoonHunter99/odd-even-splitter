a0dff49409d30664d605d6dd0ac96d4c48da2b05
Neil Daniel Vergara <vergaraneildaniel@gmail.com>
2023-04-25 15:28:17 +0800

    converting lines to integers

--- odd_even_splitter.py
+++ odd_even_splitter.py
@@ -1,9 +1,9 @@
 #open the needed files
 with open("numbers.txt" , "r") as number_file, open("even.txt" , "a") as even_file, open("odd.txt" , "a") as odd_file:
     #for loops to read the lines
-
+    for line in number_file:
         #convert the lines to integeers
-
+        number = int(line)
         #check if even
 
             #convert to sring
