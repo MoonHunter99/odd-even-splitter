3fef750cb3599c3432f6efd691b1dc9d552ccff0
Neil Daniel Vergara <vergaraneildaniel@gmail.com>
2023-04-25 15:31:41 +0800

    even checker is now working

--- odd_even_splitter.py
+++ odd_even_splitter.py
@@ -5,11 +5,11 @@ with open("numbers.txt" , "r") as number_file, open("even.txt" , "a") as even_fi
         #convert the lines to integeers
         number = int(line)
         #check if even
-
+        if number % 2 == 0:
             #convert to sring
-
+            even = str(number)
             #write to even file
-
+            even_file.write(even + "\n")
         #check if odd
 
             #convert to sring
