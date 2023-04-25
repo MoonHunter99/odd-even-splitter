adf7e6fb90d044742906f445e71f8d355f663b8a
Neil Daniel Vergara <vergaraneildaniel@gmail.com>
2023-04-25 15:26:45 +0800

    File openings

--- odd_even_splitter.py
+++ odd_even_splitter.py
@@ -1,5 +1,5 @@
 #open the needed files
-
+with open("numbers.txt" , "r") as number_file, open("even.txt" , "a") as even_file, open("odd.txt" , "a") as odd_file:
     #for loops to read the lines
 
         #convert the lines to integeers
