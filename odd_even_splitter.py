eb15e7a2550b62ddfc376bb7d64ccd84041a8dc6
Neil Daniel Vergara <vergaraneildaniel@gmail.com>
2023-04-25 15:24:11 +0800

    Pseudocode

---
+++ odd_even_splitter.py
@@ -0,0 +1,17 @@
+#open the needed files
+
+    #for loops to read the lines
+
+        #convert the lines to integeers
+
+        #check if even
+
+            #convert to sring
+
+            #write to even file
+
+        #check if odd
+
+            #convert to sring
+
+            #write to even file
